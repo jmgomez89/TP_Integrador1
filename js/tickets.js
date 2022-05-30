1d39603983c6df5f59832a88926cff901b34e958
jmgomez89 <jmgomezn8@gmail.com>
2022-05-30 18:40:39 -0300

    final

--- js/tickets.js
+++ js/tickets.js
@@ -6,20 +6,24 @@ function mostrarPrecio(){
     let cantidad = document.getElementById("cantidad").value
     let categoriaValor = document.getElementById("categoria").value
 
-
-    if(categoriaValor === 'estudiante'){
-        var total = cantidad * 40
-        precio.innerText = total
-        console.log(categoriaValor)
-    }else if(categoriaValor === 'trainee'){
-        var total = cantidad * 100
-        precio.innerText = total
-        console.log(categoriaValor)
-    }else if(categoriaValor === 'junior'){
-        var total = cantidad * 170
-        precio.innerText = total
-        console.log(categoriaValor)
+    if(cantidad != ''){
+        if(categoriaValor === 'estudiante'){
+            var total = cantidad * 40
+            precio.innerText = total
+            console.log(categoriaValor)
+        }else if(categoriaValor === 'trainee'){
+            var total = cantidad * 100
+            precio.innerText = total
+            console.log(categoriaValor)
+        }else if(categoriaValor === 'junior'){
+            var total = cantidad * 170
+            precio.innerText = total
+            console.log(categoriaValor)
+        }
+    }else{
+        precio.innerText = 'Ingrese una cantidad válida'
     }
+        
     
     
 }
