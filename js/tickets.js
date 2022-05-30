4ee13c53629e681997273e34ae83c08edf8ca5dd
jmgomez89 <jmgomezn8@gmail.com>
2022-05-29 21:26:42 -0300

    tickets

---
+++ js/tickets.js
@@ -0,0 +1,25 @@
+let resumen = document.getElementById("resumen")
+let precio = document.getElementById("precio")
+
+function mostrarPrecio(){
+
+    let cantidad = document.getElementById("cantidad").value
+    let categoriaValor = document.getElementById("categoria").value
+
+
+    if(categoriaValor === 'estudiante'){
+        var total = cantidad * 40
+        precio.innerText = total
+        console.log(categoriaValor)
+    }else if(categoriaValor === 'trainee'){
+        var total = cantidad * 100
+        precio.innerText = total
+        console.log(categoriaValor)
+    }else if(categoriaValor === 'junior'){
+        var total = cantidad * 170
+        precio.innerText = total
+        console.log(categoriaValor)
+    }
+    
+    
+}
